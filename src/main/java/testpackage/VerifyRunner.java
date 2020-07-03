9f7c016bcd25a4f22a9767f0f721d566c3fd1d96
nmittal020695 <65939616+nmittal020695@users.noreply.github.com>
2020-07-03 15:10:36 +0530

    new commit

--- src/main/java/testpackage/VerifyRunner.java
+++ src/main/java/testpackage/VerifyRunner.java
@@ -1,6 +1,10 @@
+package testpackage;
+
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+
+import com.google.common.base.Verify;
 
 public class VerifyRunner {
 	public static void main(String[] args) {
@@ -12,3 +16,4 @@ public class VerifyRunner {
 
 }
 }
+
